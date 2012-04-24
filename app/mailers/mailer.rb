02d3f89f8dbbaf830cb3d0ca35c83d08bb82e378
Will Powers <will@lokalite.com>
2012-04-24 17:11:38 -0600

    Footer style fixes and mailer.rb

---
+++ app/mailers/mailer.rb
@@ -0,0 +1,63 @@
+#TODO... Configure mailer for new app. Need to set up new email address for @recipients @from and mail
+
+class Mailer < ActionMailer::Base
+  default_url_options.merge(
+    DefaultUrlOptions
+  )
+
+  default(
+    :from => App.email
+  )
+
+  def new_event_notification(event)
+    @event   = event
+    @subject = subject_for("New event!")
+    mail(:to => 'content@lokalite.com', :subject => @subject)
+  end
+
+  def test(email)
+    mail(:to => email, :subject => 'test')
+  end
+
+  def contact_email(email_params)
+    @recipients = 'info@lokalite.com'
+    @from = email_params[:name]
+    @subject = email_params[:subject] 
+    @body["email_body"] = email_params[:comments]
+    @body["email_name"] = email_params[:name]
+    @email = email_params[:email]
+    content_type "text/html"
+  end
+
+  def advertise_email(email_params)
+    @recipients = 'info@lokalite.com'
+    @from = 'lokalite@lokalite.com' 
+    @company = email_params[:company]
+    @subscription = email_params[:subscription]
+    @featuring_events = email_params[:featuring_events]
+    @social_media = email_params[:social_media]
+    @non_profit = email_params[:non_profit]
+    @body["email_body"] = email_params[:comments]
+    @body["email_name"] = email_params[:name]
+    @budget = email_params[:budget]
+    @phone = email_params[:phone]
+    @start = email_params[:begin]
+    @first_name = email_params[:first_name]
+    @last_name = email_params[:last_name]
+    @email = email_params[:email]
+    @city = email_params[:city]
+    @agency = email_params[:agency]
+    mail(:to => 'info@lokalite.com', :subject => 'Advertising Request')
+    content_type "text/html"
+  end
+
+protected
+  def subject_for(*args)
+    ["[#{ App.domain }]", *args.compact.flatten].join(' ')
+  end
+
+  def signature
+    "-- Thanks from the #{ App.domain } team."
+  end
+  helper_method :signature
+end
