2866dd419a5f2453cd2179880c67ca2ab6373592
Will Powers <will@lokalite.com>
2012-04-24 15:45:36 -0600

    Root controller redirect fix

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,10 +1,9 @@
 class RootController < ApplicationController
   def index
-    if session[:skip_landing_page]
-      redirect_to profile_path
+    if user_signed_in? 
+      redirect_to home_path
     else
       render :action => 'homepage'
-    end
-    session[:skip_landing_page] = true
+    end 
   end
 end
